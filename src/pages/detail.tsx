197392d83afe7ab46897db382d4a79d890b95ab5
robiel <robiel.tesfazghi@cpnv.ch>
2023-01-12 09:09:55 +0100

    Update detail page to toggle the listBox

--- src/pages/detail.tsx
+++ src/pages/detail.tsx
@@ -2,24 +2,32 @@
 import { useState } from 'react';
 import pokemons from '../data/pokemon.json'; 
 import { ListBox } from 'primereact/listbox';
+import 'primeicons/primeicons.css';
+import { Button } from 'primereact/button';
+ 
  
 export default function Detail() {
 
     const [pokemon, setPokemon] = useState(null);
+    const [display, setDisplay] = useState(true);
 
-  function getPokemonName() {
-    return pokemon ? pokemon[0].name : ''
-    }
+    const getPokemonName = () => pokemon ? pokemon[0].name: ''
 
+    const displayListBox =()=> display? 'visible' :'hidden'
     
     return (
         <>
-        <h1 className='pokemonTitle'>{getPokemonName()}</h1> 
+        <Button 
+            onClick={()=>setDisplay(!display)}
+            className="p-button-text p-button-plain pokemonTitle p-button-lg">
+            {getPokemonName()} <i className="pi pi-angle-down"></i> 
+        </Button>
                  
-         <ListBox className='listbox' value={null} options={pokemons} onChange={(e) => setPokemon(e.value)} multiple filter optionLabel="name"
-                 style={{ width: '15rem' }} listStyle={{ maxHeight: '250px' }} />
-
-
+         <ListBox className='listbox' value={null} options={pokemons} 
+            onChange={(e) => {setPokemon(e.value); setDisplay(false)}} 
+            multiple filter optionLabel="name"
+            style={{ width: '15rem', visibility: displayListBox() }} 
+            listStyle={{ maxHeight: '250px' }} />
         </>
     );
 }
