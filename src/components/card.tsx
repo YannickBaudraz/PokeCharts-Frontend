90fd8543e1f00045f9b58adad1cd17c20d2cd473
Nomeos <mathieu.rabot01@gmail.com>
2023-02-09 10:40:34 +0100

    Add scale transition to the cards and link to the detail page

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,19 +1,22 @@
 import Types from "./type";
 import "/src/assets/style/card.css";
+import {Link} from "react-router-dom";
 
 export default function Card({pokemon}: any, index: number) {
     return (
-        <div id={index.toString()} className="pokemon-card">
-            <div className="pokemon-name">
-                <h1>{pokemon.name}</h1>
+        <Link to={`/detail/${pokemon.id}`}>
+            <div id={index.toString()} className="pokemon-card">
+                <div className="pokemon-name">
+                    <h1>{pokemon.name}</h1>
+                </div>
+                <img
+                    src={pokemon.sprite}
+                    alt={pokemon.name}
+                    className="pokemon-image"/>
+                <div className="pokemon-types">
+                    <Types types={pokemon.types}/>
+                </div>
             </div>
-            <img
-                src={pokemon.sprite}
-                alt={pokemon.name}
-                className="pokemon-image"/>
-            <div className="pokemon-types">
-                <Types types={pokemon.types}/>
-            </div>
-        </div>
+        </Link>
     )
 }
