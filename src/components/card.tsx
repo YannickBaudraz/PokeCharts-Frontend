359d9b80b78f6639e0940f29d8890a07c68d2b97
Nomeos <mathieu.rabot01@gmail.com>
2023-03-09 13:05:58 +0100

    OnError, replace the missing image with a placeholder image

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -7,11 +7,15 @@ export default function Card({pokemon}: any, index: number) {
         <Link to={`/detail/${pokemon.id}`}>
             <div id={index.toString()} className="pokemon-card">
                 <div className="pokemon-name">
-                    <h1>{pokemon.name}</h1>
+                    <h2>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h2>
                 </div>
                 <img
                     src={pokemon.sprites.shiny}
                     alt={pokemon.name}
+                    onError={(e: any) => {
+                        e.target.src = "src/assets/pokeball-800x600.gif"
+                    }
+                    }
                     className="pokemon-image"/>
                 <div className="pokemon-types">
                     <Types types={pokemon.types}/>
